e1e652d7894d13c4a2050915cadeddab24265b35
Steven Kraft <kraftydinosaur@gmail.com>
2018-03-24 09:23:12 -0400

    Cleaned up and reorganized

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ function clearData() {localStorage.clear();}
 
 function showIntro(){
   return botui.message.add({
-    content: `Hello! I'm ${botname}. I'm glad you chose this resource to help you learn to read Japanese.`
+    content: "Hello! I'm " + botname + ". I'm glad you chose this resource to \
+      help you learn to read Japanese."
   }).then(function () {
       return botui.message.add({
         content: 'Are you familiar with hiragana and katakana?',
@@ -27,47 +28,52 @@ function showIntro(){
         if(!res.value) {
           return botui.message.add({
             delay: 1000,
-            content: "Hiragana and katakana, collectively called kana, are two of the three writing systems used in Japanese."
+            content: "Hiragana and katakana, collectively called kana, are two\
+              of the three writing systems used in Japanese."
           }).then(function(){
             return botui.message.add({
               delay: 3000,
-              content: "Hiragana and katakana are syllabaries which represent all of the possible sounds that can be expressed in the Japanese language."
+              content: "Hiragana and katakana are syllabaries which represent \
+                all of the possible sounds that can be expressed in the \
+                Japanese language."
             }).then(function(){
                 return botui.message.add({
                   delay: 3000,
-                  content: "There are 46 of each; each hiragana has a corresponding katakana, and they sound the same. Hiragana is used to write native Japanese words, and katakana is used to write foreign loanwords."
+                  content: "There are 46 of each; each hiragana has a \
+                    corresponding katakana, and they sound the same. Hiragana \
+                    is used to write native Japanese words, and katakana is \
+                    used to write foreign loanwords."
                   })
               })
           })
         }
-      });
+      }).then(function(){
+          return botui.message.add({
+            delay: 1000,
+            content: "Great! One more thing, what's your name?"
+          })
+        });
 }
 
 function getUser() {
   // Get Name from User and store in LocalStorage
-  if (hasData()){
-    user = JSON.parse(localStorage.getItem('user')).value;
-    return botui.action.hide({});
-  } else {
-    return showIntro().then(function(){
-      return botui.message.add({
-        delay: 1000,
-        content: "Great! One more thing, what's your name?"
-      }).then(function () {
-        return botui.action.text({
+  return showIntro().then(function(){
+      return botui.action.text({
           action: {
             placeholder: 'Your name'
           }
-        });
-      }).then(function (res) {
-        localStorage.setItem('user', JSON.stringify(res));
-        user = res.value;
+        }).then(function (res) {
+          localStorage.setItem('user', JSON.stringify(res));
+          user = res.value;
       });
     });
-  }
 }
 
 var user = "";
-getUser().then(function(){
-  botui.message.add({content: 'Hello ' + user + '!'});
+if (hasData()){user = JSON.parse(localStorage.getItem('user')).value;}
+else {showIntro();}
+
+botui.message.add({
+  content: 'Hello ' + user + '!',
+  delay: 1000
 });
