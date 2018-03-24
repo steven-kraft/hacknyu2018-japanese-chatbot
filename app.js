5c3c5d833c0d8f08340199d168a17b25c8e7472b
Steven Kraft <steven.kraftjp@gmail.com>
2018-03-24 12:09:14 -0400

    Added user kanaData data structure with initialization

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var botui = new BotUI('japan-bot', {
 
 var botname = "JapanBot";
 
+var hiragana = [
+		"あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ",
+    "た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ",
+    "ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","を",
+    "ん","が","ぎ","ぐ","げ","ご","ざ","じ","ず","ぜ","ぞ","だ","ぢ","づ","で",
+    "ど","ば","び","ぶ","べ","ぼ","ぱ","ぴ","ぷ","ぺ","ぽ"
+]
+
+var kanaData = [];
+
 function hasData() {
   return localStorage.getItem('user');
 }
@@ -12,6 +22,14 @@ function clearData() {localStorage.clear();}
 
 function initKanaData() {
   //TODO Initialize Kana Data Structure in Local Storage
+  hiragana.forEach(function(h){
+      kanaData.push({
+          kana: h,
+          level: 0,
+          next_review: null
+        })
+  })
+  localStorage.setItem('kanaData', JSON.stringify(kanaData));
 }
 
 function kanaCorrect(kana) {
@@ -75,6 +93,7 @@ function getUser() {
         action: {placeholder: 'Your name'}
       }).then(function (res) {
         localStorage.setItem('user', JSON.stringify(res));
+        initKanaData();
         user = res.value;
     });
 }
