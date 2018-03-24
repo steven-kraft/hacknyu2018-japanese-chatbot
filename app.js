a3e5e32ecffdad0ea081dde6d9267dc6633fcd9d
Steven Kraft <kraftydinosaur@gmail.com>
2018-03-23 22:55:53 -0400

    Added ability to save and load user's name to local storage

--- app.js
+++ app.js
@@ -2,11 +2,36 @@ var botui = new BotUI('japan-bot', {
   vue: Vue // or this
 });
 
-botui.message.add({
-  content: 'Hello! Are you ready to learn Japanese?! 😤'
-});
+function hasData() {
+  return localStorage.getItem('user');
+}
 
-botui.message.add({
-  human: true,
-  content: 'Howdy!'
-});
+function clearData() {localStorage.clear();}
+
+function showIntro(){
+  //TODO Introduce user to the chatbot
+}
+
+function getUser() {
+  // Get Name from User and store in LocalStorage
+  if (hasData()){
+    return JSON.parse(localStorage.getItem('user')).value;
+  } else {
+    botui.message.add({
+      human: true,
+      content: 'Whats your name?'
+    }).then(function () {
+      return botui.action.text({
+        action: {
+          placeholder: 'Your name'
+        }
+      });
+    }).then(function (res) {
+      localStorage.setItem('user', JSON.stringify(res));
+      return res.value;
+    });
+  }
+}
+
+var user = getUser();
+botui.message.add({content: 'Hello ' + user + '!'});
