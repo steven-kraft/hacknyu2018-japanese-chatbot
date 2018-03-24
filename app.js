dccaad666a39ef5b3677de068de6abf7d50ed1cb
Steven Kraft <kraftydinosaur@gmail.com>
2018-03-24 00:58:33 -0400

    Fixed Intro message

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function hasData() {
 function clearData() {localStorage.clear();}
 
 function showIntro(){
-  botui.message.add({
+  return botui.message.add({
     content: `Hello! I'm ${botname}. I'm glad you chose this resource to help you learn to read Japanese.`
   }).then(function () {
       botui.message.add({
@@ -25,21 +25,20 @@ function showIntro(){
       })
     }).then(function (res) {
         if(!res.value) {
-          botui.message.add({
+          return botui.message.add({
             delay: 1000,
             content: "Hiragana and katakana, collectively called kana, are two of the three writing systems used in Japanese."
           }).then(function(){
-            botui.message.add({
-              delay: 1000,
+            return botui.message.add({
+              delay: 3000,
               content: "Hiragana and katakana are syllabaries which represent all of the possible sounds that can be expressed in the Japanese language."
             }).then(function(){
-                botui.message.add({
-                  delay: 1000,
+                return botui.message.add({
+                  delay: 3000,
                   content: " There are 46 of each; each hiragana has a corresponding katakana, and they sound the same. Hiragana is used to write native Japanese words, and katakana is used to write foreign loanwords."
                   })
               })
           })
-
         }
       });
 }
@@ -50,24 +49,25 @@ function getUser() {
     user = JSON.parse(localStorage.getItem('user')).value;
     return botui.action.hide({});
   } else {
-    return botui.message.add({
-      human: true,
-      content: "Great! One more thing, what's your name?"
-    }).then(function () {
-      return botui.action.text({
-        action: {
-          placeholder: 'Your name'
-        }
+    return showIntro().then(function(){
+      return botui.message.add({
+        delay: 1000,
+        content: "Great! One more thing, what's your name?"
+      }).then(function () {
+        return botui.action.text({
+          action: {
+            placeholder: 'Your name'
+          }
+        });
+      }).then(function (res) {
+        localStorage.setItem('user', JSON.stringify(res));
+        user = res.value;
       });
-    }).then(function (res) {
-      localStorage.setItem('user', JSON.stringify(res));
-      user = res.value;
     });
   }
 }
 
 var user = "";
-showIntro();
 getUser().then(function(){
   botui.message.add({content: 'Hello ' + user + '!'});
 });
